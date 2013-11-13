e93097c94f66a8909c064268f9e7177fa68cfb7d
Joseph Boman <jboman@usc.edu>
2013-11-13 11:40:28 -0800

    Added the Role and changed TellerAgent to TellerRole

---
+++ src/bank/BankTellerRole.java
@@ -0,0 +1,12 @@
+package bank;
+
+/**
+ * class BankTellerRole
+ * This role is the bank teller, who manages requests from people to access and 
+ * change their current bank account info
+ * @author Joseph
+ *
+ */
+public class BankTellerRole {
+
+}
