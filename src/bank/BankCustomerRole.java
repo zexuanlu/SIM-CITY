419e57010d6af9582972c7cfe9b500061177a617
Joseph Boman <jboman@usc.edu>
2013-11-15 17:01:27 -0800

    Added CustomerRole stuff

--- src/bank/BankCustomerRole.java
+++ src/bank/BankCustomerRole.java
@@ -1,7 +1,67 @@
 package bank;
 
-import bank.interfaces.BankCustomer;
+import bank.interfaces.*;
+import agent.*;
+import java.util.*;
 
-public class BankCustomerRole implements BankCustomer {
-
+public class BankCustomerRole extends Role implements BankCustomer {
+	//Data
+	String name;
+	List<Task> tasks;
+	int accountNumber;
+	double balance;
+	BankTeller bt;
+	state s;
+	
+	public BankCustomerRole(String name){
+		this.name = name;
+		tasks = new ArrayList<Task>();
+	}
+	
+	//Messages
+	
+	public void msgGoToBank(String task, double amount){
+		tasks.add(new Task(task, amount));
+		s = state.needTeller;
+		stateChanged();
+	}
+	
+	public void msgHereIsTeller(BankTeller bt){
+		this.bt = bt;
+		s = state.haveTeller;
+		stateChanged();
+	}
+	
+	//Scheduler
+	protected boolean pickAndExecuteAnAction(){
+		if(s == state.needTeller){
+			informHost();
+			return true;
+		}
+		else if(s == state.haveTeller){
+			//goToLocation("Teller");
+			s = state.atTeller;
+			return true;
+		}
+		return false;
+	}
+	//Actions
+	private void informHost(){
+		//goToLocation("Host");
+		//bh.iNeedTeller();
+		s = state.waiting;
+	}
+	
+	
+	//Utilities
+	class Task{
+		String type;
+		double amount;
+		Task(String type, double amount){
+			this.type = type;
+			this.amount = amount;
+		}
+	}
+	
+	enum state {needTeller, waiting, haveTeller, atTeller, runningAway, none}
 }
