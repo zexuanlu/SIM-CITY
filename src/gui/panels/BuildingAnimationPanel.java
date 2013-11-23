d32899b88dc26a84f5b7905af8e583d8ab2f4450
Joseph Boman <jboman@usc.edu>
2013-11-23 15:53:16 -0800

    Removed excess testing code

--- src/gui/panels/BuildingAnimationPanel.java
+++ src/gui/panels/BuildingAnimationPanel.java
@@ -7,7 +7,6 @@ package gui.panels;
 
 import javax.swing.*;
 import java.awt.*;
-import bank.gui.*;
 
 
 public class BuildingAnimationPanel extends JPanel {
@@ -15,18 +14,13 @@ public class BuildingAnimationPanel extends JPanel {
 	private String title = " Building Animation ";
 	public static final int WIDTH = CityAnimationPanel.WIDTH;
 	public static final int HEIGHT = CityAnimationPanel.HEIGHT;
-	BankAnimationPanel aniPanel = new BankAnimationPanel();
 	
 	public BuildingAnimationPanel() {
 		// PANEL SETUP
 		this.setBorder(BorderFactory.createTitledBorder(title));
-		this.setLayout(new BorderLayout());
 		
 		// Panel size initiations
 		this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-		this.add(aniPanel);
-		aniPanel.setPreferredSize(this.getSize());
-		aniPanel.setVisible(true);
 	}
 
 }
