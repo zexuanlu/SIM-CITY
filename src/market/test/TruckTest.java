ad2714e42dec20557042d27623c3de7590b4db02
zexuanlu <zexuanlu@usc.edu>
2013-11-26 22:22:52 -0800

    truck test

--- src/market/test/TruckTest.java
+++ src/market/test/TruckTest.java
@@ -3,9 +3,10 @@ package market.test;
 import junit.framework.TestCase;
 import market.Food;
 import market.MarketTruckAgent;
-import market.MarketTruckAgent.CarState;
+import market.MarketTruckAgent.state;
 import restaurant.*;
 import restaurant.test.mock.MockRestaurantCook;
+import market.gui.MarketTruckGui;
 import market.test.mock.EventLog;
 import market.test.mock.MockCashier;
 
@@ -19,12 +20,12 @@ public class TruckTest extends TestCase{
 	MarketTruckAgent truck;
 	MockRestaurantCook cook;
 	MockCashier cashier;
-	AStarTraversal aStar;
+	MarketTruckGui truckGui;
 	
 	public void setUp() throws Exception{
 		super.setUp();
-		aStar = new AStarTraversal(null);
-		truck = new MarketTruckAgent(aStar);
+		truck = new MarketTruckAgent();
+		truckGui = new MarketTruckGui(truck);
 		cook = new MockRestaurantCook("cook");
 		cashier = new MockCashier("C");
 		
@@ -34,17 +35,33 @@ public class TruckTest extends TestCase{
 		
 		truck.setCashier(cashier);
 		
+		truck.setGui(truckGui);
+		
 		List<Food> food = new ArrayList<Food>();
 		
 		truck.gotoPosition(cook,food, 200, 200);
 		
 		assertTrue("truck should contain a state == sending. It doesn't.",
-				truck.carstate == CarState.goTo);
+				truck.s == state.collecting);
+		
+		truck.msgrelease();
 		
 		assertTrue("truck's scheduler should have returned true, but didn't.", truck.pickAndExecuteAnAction());
 		
 		assertTrue("truck should contain a state == sending. It doesn't.",
-				truck.carstate == CarState.moving);
+				truck.s == state.sending);
+		
+		truck.msgGoBack();
+		
+		assertTrue("truck should contain a state == sending. It doesn't.",
+				truck.s == state.readytoback);
+		
+		truck.msgrelease();
+		
+		assertTrue("truck's scheduler should have returned true, but didn't.", truck.pickAndExecuteAnAction());
+		
+		assertTrue("truck should contain a state == back. It doesn't.",
+				truck.s == state.back);
 		
 	}
 	
