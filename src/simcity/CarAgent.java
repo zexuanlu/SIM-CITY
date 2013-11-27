bd6418c554537a4cb51202b97101cd4fa33dd8e1
lizhifan <lizhifan@usc.edu>
2013-11-26 22:08:07 -0800

    closes issues closes #39 closes #38 closes #37 closes #36 closes #35 closes #34 closes #33 closes #32 closes #31 closes #30

--- src/simcity/CarAgent.java
+++ src/simcity/CarAgent.java
@@ -170,7 +170,7 @@ public class CarAgent extends Agent {
  
  public void deadPos(int x, int y){
       currentPosition.release(aStar.getGrid());
-             currentPosition = new Position(x/scale, y/scale);
+      currentPosition = new Position(x/scale, y/scale);
      currentPosition.moveInto(aStar.getGrid());
      originalPosition = currentPosition;
  }
