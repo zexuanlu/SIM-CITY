f6bb1eba85280137b5fcdfc7ec29eb893552182d
zexuanlu <zexuanlu@usc.edu>
2013-12-06 23:06:26 -0800

    Icon Test

--- src/gui/panels/CityAnimationPanel.java
+++ src/gui/panels/CityAnimationPanel.java
@@ -52,6 +52,19 @@ public class CityAnimationPanel extends JPanel implements ActionListener, MouseL
 	public Restaurant1AnimationPanel rest1Panel = new Restaurant1AnimationPanel();
 	public Restaurant6AnimationPanel rest6Panel = new Restaurant6AnimationPanel();
 	
+	public ImageIcon img = new ImageIcon("image/market1.png");
+	public Image m1 = img.getImage();
+	public ImageIcon img1 = new ImageIcon("image/bank.png");
+	public Image b = img1.getImage();
+	public ImageIcon img2 = new ImageIcon("image/house1.png");
+	public Image h1 = img2.getImage();
+	public ImageIcon img3 = new ImageIcon("image/market2.png");
+	public Image m2 = img3.getImage();
+	public ImageIcon img4 = new ImageIcon("image/house2.png");
+	public Image h2 = img4.getImage();
+	public ImageIcon img5 = new ImageIcon("image/house3.png");
+	public Image h3 = img5.getImage();
+	
 	// Creating the apartment panels
 //	public ApartmentAnimationPanel apt1Panel = new ApartmentAnimationPanel(5);
 //	public ApartmentAnimationPanel apt2Panel = new ApartmentAnimationPanel(6);
@@ -292,6 +305,11 @@ public class CityAnimationPanel extends JPanel implements ActionListener, MouseL
         g2.fillRect(280, 0, 60, 480);
         g2.fillRect(0, 220, 640, 60);
         
+        g2.drawImage(b, 140, 160, BUILDINGSIZE, BUILDINGSIZE, BuildPanel);
+        g2.drawImage(m1, 220, 160, BUILDINGSIZE, BUILDINGSIZE, BuildPanel);
+        g2.drawImage(h1, 340, 160, BUILDINGSIZE, BUILDINGSIZE, BuildPanel);
+        g2.drawImage(h2, 340, 80, BUILDINGSIZE, BUILDINGSIZE, BuildPanel);
+        g2.drawImage(h3, 540, 160, BUILDINGSIZE, BUILDINGSIZE, BuildPanel);
         
         //Hover Text
     	g2.setColor(Color.BLACK);
