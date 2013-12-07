97db1758382ef8e1f53e61ff44215559b017ec4e
Grant Collins <grantcol@usc.edu>
2013-12-07 14:24:28 -0800

    person gui avoidance

--- src/person/gui/PersonGui.java
+++ src/person/gui/PersonGui.java
@@ -6,32 +6,40 @@ import java.awt.Graphics2D;
 import person.PersonAgent;
 import person.Position;
 import utilities.Gui;
+import utilities.TrafficLightAgent;
 
 public class PersonGui implements Gui{
 	private PersonAgent agent = null;
-
+	private TrafficLightAgent light = null;
 	public int xPos = -20, yPos = -20;//default player position
 	public int xDestination = 20, yDestination = 20;//default start position
 	private boolean arrived; 
 	public boolean isPresent;
-
+	public boolean atLight;
+	
 	public PersonGui(PersonAgent agent) {
 		this.agent = agent;
 		arrived = false;
 		isPresent = true;
 	}
-
+	public PersonGui(PersonAgent agent, TrafficLightAgent tlight){
+		light = tlight;
+		this.agent = agent;
+		arrived = false;
+		isPresent = false;
+		atLight = false;
+	}
 	public void updatePosition() {
-		if (xPos < xDestination){ 
+		if (xPos < xDestination){ //&& (yPos < 200 + 70 || yPos > 200 - 70)){ 
 			xPos++; 
 		}
-		else if (xPos > xDestination){ 
+		else if (xPos > xDestination){// && (yPos < 200 + 70 || yPos > 200 - 70)){ 
 			xPos--; 
 		}
-		if (yPos < yDestination){ 
+		if (yPos < yDestination ){//&& (xPos < 360 + 70 || xPos > 360 - 70)){ 
 			yPos++; 
 		}
-		else if (yPos > yDestination){ 
+		else if (yPos > yDestination ){//{&& (xPos < 360 + 70 || xPos > 360 - 70)){ 
 			yPos--; 
 		}
 		if(yPos == yDestination && xPos == xDestination && !arrived){
@@ -67,7 +75,9 @@ public class PersonGui implements Gui{
 		yPos = y; 
 		arrived = false; 
 	}
-	
+	public void cross(){
+		light.msgCheckLight(this.agent);
+	}
 	public int getX(){return xPos;}
 	public int getY(){return yPos;}
 	public void setStart(int x, int y){
