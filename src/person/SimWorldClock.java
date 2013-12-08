b6b254de6d22aa45f1ff8383a22e88f3ea7b6cf8
Joseph Boman <jboman@usc.edu>
2013-12-07 18:59:31 -0800

    Changed the length of time of 1 hour

--- src/person/SimWorldClock.java
+++ src/person/SimWorldClock.java
@@ -27,7 +27,7 @@ public class SimWorldClock {
 			public void run() {
 				updateWorldClock();
 			}
-		}, 0, 3000); // one minute per hour 
+		}, 0, 6000); // one minute per hour 
 	}
 	private void updateWorldClock(){
 		System.out.println("Time is now " + currentHour );
