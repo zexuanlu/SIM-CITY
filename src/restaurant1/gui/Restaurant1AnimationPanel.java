85dcaf8e8c855588ee7166e387385164629518d3
zexuanlu <zexuanlu@usc.edu>
2013-12-09 14:43:45 -0800

    restaurant1 gui fixed

--- src/restaurant1/gui/Restaurant1AnimationPanel.java
+++ src/restaurant1/gui/Restaurant1AnimationPanel.java
@@ -71,11 +71,11 @@ public class Restaurant1AnimationPanel extends JPanel implements ActionListener 
         
         // Drawing the fridge
         g2.setColor(Color.BLUE);
-        g2.fillRect(500, 160, 20, 20);
+        g2.fillRect(430, 160, 20, 20);
         
 		g2.setColor(Color.cyan);
-		g2.fillRect(540, 230, 30, 30);
-		g2.fillRect(540, 130, 30, 30);
+		g2.fillRect(470, 230, 30, 30);
+		g2.fillRect(470, 130, 30, 30);
      
         if(checkx != xc || checky!= yc ){
             g2.setColor(Color.ORANGE);
