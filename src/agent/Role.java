de1ea6fe2e1a1055584b9f9d1d7ee2cc472932d7
zexuanlu <zexuanlu@usc.edu>
2013-12-10 12:14:48 -0800

    Adding in Jennie's changes to homeowner

--- src/agent/Role.java
+++ src/agent/Role.java
@@ -42,7 +42,7 @@ public abstract class Role {
             this.isActive = active;
     }
 
-    /**
+   /**
 * This should be called whenever state has changed that might cause
 * the agent to do something.
 */
