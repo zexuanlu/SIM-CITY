03fea0321736b64d5bcb80d6d11928c5a5e8ce24
Grant Collins <grantcol@usc.edu>
2013-12-10 12:43:11 -0800

    Market changes

--- src/market/gui/MarketTruckGui.java
+++ src/market/gui/MarketTruckGui.java
@@ -24,6 +24,8 @@ public class MarketTruckGui implements Gui {
 
 	private MarketTruckAgent myTruck;
 	BuildingAnimationPanel buildPanel;
+	private int xorigin = 150, yorigin = 90;
+	private int x1origin  = 510, y1origin = 90;
 	private int x1rest = 220;
 	private int y1rest = 90;
 	private int x2rest = 250, y2rest = 90;
@@ -109,6 +111,18 @@ public class MarketTruckGui implements Gui {
 
 		if (xPos == xDestination &&atstop&& yPos == yDestination
 				& (xDestination == x6rest) & (yDestination == y6rest )) {
+			atstop = false;
+			myTruck.msgrelease();
+		}
+
+		if (xPos == xDestination &&atstop&& yPos == yDestination
+				& (xDestination == xorigin) & (yDestination == yorigin )) {
+			atstop = false;
+			myTruck.msgrelease();
+		}
+
+		if (xPos == xDestination &&atstop&& yPos == yDestination
+				& (xDestination == x1origin) & (yDestination == y1origin )) {
 			atstop = false;
 			myTruck.msgrelease();
 		}
